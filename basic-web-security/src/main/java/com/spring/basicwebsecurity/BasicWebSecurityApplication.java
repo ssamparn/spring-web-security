498cd41322dc9f702d927a7bf1c2bf780e5860d9
samantrs <https://samantrs@git.rabobank.nl>
2020-05-02 22:56:43 +0200

    First commit

---
+++ basic-web-security/src/main/java/com/spring/basicwebsecurity/BasicWebSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.spring.basicwebsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicWebSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicWebSecurityApplication.class, args);
+	}
+
+}
---
+++ basic-web-security/src/test/java/com/spring/basicwebsecurity/BasicWebSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.basicwebsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicWebSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
