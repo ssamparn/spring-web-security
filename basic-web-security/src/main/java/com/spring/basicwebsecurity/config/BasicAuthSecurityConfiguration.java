5067a2c1dfad2f8c2ae124844f3955ad042a0c8b
samantrs <https://samantrs@git.rabobank.nl>
2020-05-10 20:27:44 +0200

    Configured Thymeleaf in Spring Security

--- basic-web-security/src/main/java/com/spring/basicwebsecurity/config/BasicAuthSecurityConfiguration.java
+++ basic-web-security/src/main/java/com/spring/basicwebsecurity/config/BasicAuthSecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
@@ -27,17 +28,35 @@ public class BasicAuthSecurityConfiguration extends WebSecurityConfigurerAdapter
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic();
-    }
-
-    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.jdbcAuthentication()
                 .dataSource(dataSource)
                 .passwordEncoder(getPasswordEncoder());
     }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//         httpBasic Authentication has to be commented as I am going to use form based authentication.
+//         http.authorizeRequests()
+//                 .antMatchers("/h2-console/**").permitAll()
+//                 .anyRequest().authenticated()
+//                 .and()
+//                 .httpBasic();
+
+        http.authorizeRequests()
+                .antMatchers("/secureLogin", "/h2-console/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/secureLogin")
+                .defaultSuccessUrl("/employeeHome", true)
+                .and()
+                .csrf().disable();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/css/**", "/webjars/**");
+    }
+
+
 }
---
+++ basic-web-security/src/main/java/com/spring/basicwebsecurity/config/ViewConfiguration.java
@@ -0,0 +1,15 @@
+package com.spring.basicwebsecurity.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ViewConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/secureLogin").setViewName("secureLogin");
+    }
+
+}
