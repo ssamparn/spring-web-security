855f8dee9447274ded040e08cfd9e5a27805423c
samantrs <https://samantrs@git.rabobank.nl>
2023-11-02 22:35:04 +0100

    added asta bank security module

---
+++ asta-bank-security/src/main/java/com/spring/astabanksecurity/AstaBankSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.spring.astabanksecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstaBankSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstaBankSecurityApplication.class, args);
+	}
+
+}
---
+++ asta-bank-security/src/test/java/com/spring/astabanksecurity/AstaBankSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.astabanksecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstaBankSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
